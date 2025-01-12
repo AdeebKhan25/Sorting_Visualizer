a76179b47cc70f52f9548725db6a95df1453af46
Adeeb Khan <adeebkhankhan25@gmail.com>
2025-01-12 15:32:01 +0530

    Changed direcory structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SortingAlgorithmProvider } from "@/context/visualizer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Sorting Visualizer",
@@ -26,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={` antialiased`}
       >
         <SortingAlgorithmProvider>{children}</SortingAlgorithmProvider>
       </body>
